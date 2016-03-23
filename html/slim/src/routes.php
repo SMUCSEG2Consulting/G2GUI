e0308c16cb8fd4c4eaecc7a736b470308403d0aa
Ian Johnson <ianjjohnson@icloud.com>
2016-03-22 19:30:04 -0500

    Re-add routes.php

---
+++ html/slim/src/routes.php
@@ -0,0 +1,253 @@
+<?php
+// Routes
+
+/*
+Random bitstring generation for hash salting
+*/
+function randomBitString($length = 256) {
+    $chars = '0123456789abcdef';
+    $len = strlen($chars);
+    $str = '';
+
+    for ($i = 0; $i < $len; $i++) {
+        $str .= $chars[rand(0, $len - 1)];
+    }
+    return $str;
+}
+
+$app->get('/hello', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/goodbye', 
+	function ($request, $response, $args) {
+		return $response->write("Time to go. Goodbye!");
+	}
+);
+
+$app->get('/json',
+	function ($request, $response, $args) {
+		return $response->write(json_encode(array('field' => 'value')));
+	}
+);
+
+$app->get('/json/{id}',
+	function ($request, $response, $args){
+		return $response->write(json_encode(array('id' => $args['id'])));
+	}
+);
+
+
+$app->get('/games',
+	function($request, $response, $args) {
+		$db = $this->dbConn;
+		$statement = $db->prepare('SELECT * FROM game');
+		$statement->execute();
+		$arr = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $response->write(json_encode($arr));
+	}
+);
+
+$app->get('/game/{id}',
+	function ($request, $response, $args){
+		$db = $this->dbConn;
+
+		$statement = $db->prepare('SELECT * FROM game WHERE id=:id');
+		$statement->execute(array('id' => $args['id']));
+		$arr = $statement->fetch(PDO::FETCH_ASSOC);
+
+		$statement = $db->prepare('SELECT playerName FROM enlist WHERE gameID=:id');
+		$statement->execute(array('id' => $args['id']));
+		$temp = array_values($statement->fetchAll(PDO::FETCH_ASSOC));
+		
+		$ids = array();
+		foreach($temp as $player){
+			$ids[] = $player['playerName'];
+		}
+
+		$arr['playerNames'] = $ids;
+		
+		return $response->write(json_encode($arr));
+	}
+);
+
+$app->get('/newUser/{name}/{pwd}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+
+		$statement = $db->prepare('SELECT * FROM user WHERE name=:usr');
+		$statement->execute(array('usr'=>$args['name']));
+		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		if(!empty($result)){
+			return $response->write('Error - name already taken');
+		}
+
+		$salt = randomBitString();
+		$hash = hash('sha256', $args['pwd'] . $salt);
+
+		$statement = $db->prepare('INSERT INTO user(name, salt, hash) values(:usr, :sl, :hs)');
+		$statement->execute(array(
+			'usr' => $args['name'],
+			'sl' => $salt,
+			'hs' => $hash
+		));
+
+		return $response->write($args['name']);
+	}
+);
+
+$app->get('/users',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+		$statement = $db->prepare('SELECT name, sport1, sport2, sport3 FROM user');
+		$statement->execute();
+		$arr = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $response->write(json_encode($arr));
+	}
+);
+
+$app->get('/user/{username}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+		$statement = $db->prepare('SELECT name, sport1, sport2, sport3 FROM user WHERE name = :usr');
+		$statement->execute(array(
+				'usr' => $args['username']
+			));
+		$arr = $statement->fetchAll(PDO::FETCH_ASSOC);
+		return $response->write(json_encode($arr));
+	}
+);
+
+$app->get('/deleteUser/{username}/{password}', 
+	function($request, $response, $args){
+		
+		$db = $this->dbConn;
+
+		$statement = $db->prepare('SELECT * FROM user WHERE name=:usr');
+		$statement->execute(array('usr'=>$args['username']));
+		
+		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		if(empty($result)){
+			return $response->write('No such user');
+		}
+
+		$statement = $db->prepare('SELECT salt, hash FROM user WHERE name = :nm');
+		$statement->execute(array(
+			'nm' => $args['username'],
+		));
+
+		$item = $statement->fetch(PDO::FETCH_ASSOC);
+		$salt = $item['salt'];
+
+		$hash = hash('sha256', $args['password'] . $salt);
+
+		if($hash == $item['hash']){
+			$statement = $db->prepare('DELETE FROM user WHERE name=:name');
+			$statement->execute(array('name' => $args['username']));
+			return $response->write('Deleted!');
+		} else {
+			return $response->write('wrong pass');
+		}
+
+
+			
+	}
+);
+
+$app->get('/deleteGame/{id}',
+	function($request, $response, $args){
+		
+		$db = $this->dbConn;
+		$statement = $db->prepare('DELETE FROM game WHERE id=:id');
+		$statement->execute(array('id' => $args['id']));
+		return $response->write('Deleted.'); 
+
+	}
+
+);
+
+
+$app->get('/createGame/{hostName}/{time}/{sport}/{location}/{playerCount}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+		$statement = $db->prepare('INSERT INTO game(sport, time, playerCount, location) values (:sport, :time, :count, :loc)');
+		$statement->execute(array(
+				'sport' => $args['sport'],
+				'time' => $args['time'],
+				'loc' => $args['location'],
+				'count' => $args['playerCount']
+		));
+
+		$select = $db->prepare('SELECT max(id) from game');
+		$select->execute();
+		$id = $select->fetch(PDO::FETCH_ASSOC)['max(id)'];
+
+		$statement = $db->prepare('INSERT INTO enlist(playerName, gameID) values(:name, :id)');
+		$statement->execute(array(
+				'name' => $args['hostName'],
+				'id' => $id
+		));
+
+		return $response->write("Success!");
+	}
+);
+
+$app->get('/updateUser/{usr}/{s1}/{s2}/{s3}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+		$statement = $db->prepare('UPDATE user SET sport1= :one, sport2 = :two, sport3 = :three WHERE name = :username');
+		$statement->execute(array(
+				'one' => $args['s1'],
+				'two' => $args['s2'],
+				'three' => $args['s3'],
+				'username' => $args['usr']
+		));
+		return $response->write(json_encode($args));
+	}
+);
+
+$app->get('/login/{name}/{pwd}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+
+		$statement = $db->prepare('SELECT * FROM user WHERE name=:usr');
+		$statement->execute(array('usr'=>$args['name']));
+		$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+		if(empty($result)){
+			return $response->write('failed');
+		}
+
+		$statement = $db->prepare('SELECT salt, hash FROM user WHERE name = :nm');
+		$statement->execute(array(
+			'nm' => $args['name'],
+		));
+
+		$item = $statement->fetch(PDO::FETCH_ASSOC);
+		$salt = $item['salt'];
+
+		$hash = hash('sha256', $args['pwd'] . $salt);
+
+		if($hash == $item['hash']){
+			return $response->write("success");
+		} else {
+			return $response->write('failed');
+		}
+	}
+);
+
+
+$app->get('/addUserToGame/{gameID}/{username}',
+	function($request, $response, $args){
+		$db = $this->dbConn;
+		$statement = $db->prepare('INSERT into enlist(playerName, gameID) values(:username, :gameID)');
+		$statement->execute(array(
+				'username' => $args['username'],
+				'gameID' => $args['gameID']
+		));
+		return $response->write(json_encode($args));
+	}
+);
