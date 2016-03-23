26b66fa853d91c1a1265c3059fd7f7aa9a1f6fc6
Ian Johnson <ianjjohnson@icloud.com>
2016-03-22 20:24:51 -0500

    Remove default slim endpoints

--- html/src/routes.php
+++ html/src/routes.php
@@ -15,20 +15,6 @@ function randomBitString($length = 256) {
     return $str;
 }
 
-$app->get('/hello', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-
-$app->get('/goodbye', 
-	function ($request, $response, $args) {
-		return $response->write("Time to go. Goodbye!");
-	}
-);
-
 $app->get('/json',
 	function ($request, $response, $args) {
 		return $response->write(json_encode(array('field' => 'value')));
